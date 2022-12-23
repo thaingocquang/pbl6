3876888f2d73576171dd90a069f0c2e38c7dbd15
thaingocquang <quangthai@selly.vn>
2022-12-23 10:40:11 +0700

    fix csrf

--- music/music/settings.py
+++ music/music/settings.py
@@ -146,3 +146,9 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
