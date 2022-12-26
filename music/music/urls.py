06ccc648e625eea5ae8f51b55bfb2908d880780e
thaingocquang <quangthai@selly.vn>
2022-12-26 21:50:22 +0700

    redict admin

--- music/music/urls.py
+++ music/music/urls.py
@@ -19,6 +19,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,11 +40,12 @@ admin.site.site_header = 'Music Administration'
 from . import settings
 
 
-
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('', lambda request: redirect('/admin', permanent=False)),
 
     path('admin/', admin.site.urls),
     path('singer/', include('singer.urls')),
